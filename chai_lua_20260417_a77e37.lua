7fa78a7df653f1f182240f4b5fb5e6e615b3f1cd
saldychesskilat-sketch <saldychesskilat@gmail.com>
2026-06-04 23:46:39 +0800

    Update chai_lua_20260417_a77e37.lua

--- chai_lua_20260417_a77e37.lua
+++ chai_lua_20260417_a77e37.lua
@@ -1967,7 +1967,7 @@ local function autoParryLoop()
     mainCircle.Color = Color3.fromRGB(255,140,0)
 
     -- lebih transparan
-    mainCircle.Transparency = 0.48
+    mainCircle.Transparency = 0.87
 
     mainCircle.Anchored = true
     mainCircle.CanCollide = false
@@ -1989,7 +1989,7 @@ local function autoParryLoop()
 
     outlineCircle.Color = Color3.fromRGB(255,255,255)
 
-    outlineCircle.Transparency = 0.19
+    outlineCircle.Transparency = 0.87
 
     outlineCircle.Anchored = true
     outlineCircle.CanCollide = false
@@ -2271,7 +2271,7 @@ local function autoParryLoop()
 
         -- posisi esp dinaikkan 2 studs
         local targetPos =
-            localRootPart.Position + Vector3.new(0,0,0)
+            localRootPart.Position + Vector3.new(0,0.4,0)
 
         local espCF =
             CFrame.new(targetPos)
@@ -2339,7 +2339,7 @@ local function autoParryLoop()
                             local velocity =
                                 root.AssemblyLinearVelocity.Magnitude
 
-                            if velocity > 24 then
+                            if velocity > 194 then
 
                                 triggerParry(
                                     "VelocitySpike",

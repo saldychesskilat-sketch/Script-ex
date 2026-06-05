5b60c01b9e29b6d5dd8f5edb74afbb2673b391c3
saldychesskilat-sketch <saldychesskilat@gmail.com>
2026-06-06 00:20:33 +0800

    Update chai_lua_20260417_a77e37.lua

--- chai_lua_20260417_a77e37.lua
+++ chai_lua_20260417_a77e37.lua
@@ -3568,7 +3568,7 @@ local function createHomeContent()
     logo.Size=UDim2.new(1,-8,1,-8)
     logo.Position=UDim2.new(0,4,0,4)
     logo.BackgroundTransparency=1
-    logo.Image="rbxassetid://76051161792347"
+    logo.Image="rbxassetid://127173186832760"
     logo.ScaleType=Enum.ScaleType.Fit
     logo.Parent=logoHolder
 

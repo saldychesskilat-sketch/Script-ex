d483174d468e94064d914ba79e8eec48766ac136
saldychesskilat-sketch <saldychesskilat@gmail.com>
2026-09-14 17:40:50 +0800

    Update chai_lua_20260417_a77e37.lua

--- chai_lua_20260417_a77e37.lua
+++ chai_lua_20260417_a77e37.lua
@@ -3749,16 +3749,22 @@ local function startAutoAim()
     end
 
     -- ========== FUNGSI NEVER MISS ==========
+    -- ========== FUNGSI FIRE REMOTE ==========
     local function getFireRemotes()
         local remotes = ReplicatedStorage:FindFirstChild("Remotes")
-        if not remotes then return nil, nil end
+        if not remotes then return nil, nil, nil end
         local items = remotes:FindFirstChild("Items")
-        if not items then return nil, nil end
+        if not items then return nil, nil, nil end
         local twist = items:FindFirstChild("Twist of Fate")
-        if not twist then return nil, nil end
+        if not twist then return nil, nil, nil end
         local fireRemote = twist:FindFirstChild("Fire")
         local resultRemote = twist:FindFirstChild("Result")
-        return fireRemote, resultRemote
+        local character = game:GetService("Players").LocalPlayer.Character
+        local gun = character
+        and character:FindFirstChild("Twist of Fate")
+        and character["Twist of Fate"]:FindFirstChild("Right Arm")
+        and character["Twist of Fate"]["Right Arm"]:FindFirstChild("gun")
+        return fireRemote, resultRemote, gun
     end
 
     local function fireInfShot()

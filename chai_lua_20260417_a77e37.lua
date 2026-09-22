ec954c6d89cd1913700bc8cea76161a88ecf2bdd
saldychesskilat-sketch <saldychesskilat@gmail.com>
2026-09-22 16:01:10 +0800

    Update chai_lua_20260417_a77e37.lua

--- chai_lua_20260417_a77e37.lua
+++ chai_lua_20260417_a77e37.lua
@@ -3431,7 +3431,7 @@ local function InitializeAutobuy()
         
         -- ===== STATE ROTATION DELAY (aktif setelah trigger pertama) =====
         local rotationLockUntil = 0
-        local ROTATION_LOCK_DELAY = 0.12   -- jeda sebelum rotate ke goal baru (detik)
+        local ROTATION_LOCK_DELAY = 1   -- jeda sebelum rotate ke goal baru (detik)
         -- ================================================================
         
         VisibilityConnection = check:GetPropertyChangedSignal("Visible"):Connect(function()                    
